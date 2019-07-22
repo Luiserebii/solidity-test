f0e2187bcf476b5a8f906700c2d8835ea0c609c6
Luiserebii <luis@serebii.io>
2019-07-21 21:08:21 -0400

    Adding basic description to errors.js as well as sample string

--- errors.js
+++ errors.js
@@ -1,8 +1,16 @@
-/** Common error strings, for convenience */
+/** 
+
+Common error strings, for convenience. Idea is that user can simply throw these into, say, checking for Solidity reverts, and not worry about Googling the actual error string
+
+*/
 
 const errors = {
 
+  openzeppelin: {
 
+    ownable: 'Ownable: caller is not the owner'
+
+  }
 
 
 
